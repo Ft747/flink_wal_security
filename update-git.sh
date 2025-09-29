4c454b6b0b361f2a12757c5c32c8546a1427c915
Dtome5 <azogudaniel00@gmail.com>
2025-09-29 12:44:48 +0100

    2025-09-29

--- update-git.sh
+++ update-git.sh
@@ -1,5 +1,2 @@
 !#bin/sh
-jj commit -m "2025-09-29" 
-jj describe -m "2025-09-29" 
-jj bookmark set main
-jj git push
+jj commit -m "2025-09-29" && jj describe -m "2025-09-29" && jj bookmark set main && jj git push
