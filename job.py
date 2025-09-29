937eb4806a96d5235a7f84f3d4603cb95366ca13
Dtome5 <azogudaniel00@gmail.com>
2025-09-29 11:57:23 +0100

    2025-09-29

--- job.py
+++ job.py
@@ -1,4 +1,5 @@
-import logging
+import random
+import string
 from pyflink.datastream import StreamExecutionEnvironment, CheckpointingMode
 from pyflink.datastream.functions import (
     KeyedProcessFunction,
@@ -20,15 +21,38 @@ class WordCounter(KeyedProcessFunction):
             current = 0
         current += 1
         self.count_state.update(current)
-        yield current
+        yield (value, current)
+
+
+def random_words():
+    """Generate a random combination of five letters (uppercase + lowercase)."""
+    return "".join(random.choice(string.ascii_letters) for _ in range(3))
+
+
+def generate_words(count, vocab=None):
+    """
+    Generate a fixed number of words from a vocabulary.
+    If no vocabulary is provided, generate random five-letter words.
+
+    :param count: how many words to generate
+    :param vocab: list of words to choose from
+    :return: list of generated words
+    """
+    if vocab is None:
+        # Instead of a fixed list, generate random five-letter words
+        return [random_words() for _ in range(count)]
+
+    return [random.choice(vocab) for _ in range(count)]
 
 
 def main():
     env = StreamExecutionEnvironment.get_execution_environment()
     env.enable_checkpointing(500, CheckpointingMode.AT_LEAST_ONCE)
-    ds = env.from_collection(list(range(20)), type_info=Types.INT())
+    ds = env.from_collection(generate_words(20000), type_info=Types.STRING())
     keyed_state = ds.key_by(lambda x: x)
-    res = keyed_state.process(WordCounter(), output_type=Types.INT())
+    res = keyed_state.process(
+        WordCounter(), output_type=Types.TUPLE([Types.STRING(), Types.INT()])
+    )
     res.print()
     env.execute("Word Counter")
 
