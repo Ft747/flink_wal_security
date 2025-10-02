9b5a46083e860540c4d5ba76c5be3479faf227bd
Dtome5 <azogudaniel00@gmail.com>
2025-10-02 16:17:14 +0100

    2025-10-02 16:17

--- update-git.sh
+++ update-git.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 now=$(date +"%Y-%m-%d %H:%M")
-jj commit -m "$now" \
-  && jj describe -m "$now" \
-  && jj bookmark set master \
-  && jj git push
+jj commit -m "$now" 
+jj describe -m "$now" 
+jj bookmark set master 
+jj git push
